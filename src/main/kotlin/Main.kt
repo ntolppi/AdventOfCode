3d4a23cbb900dfafbd1d9c89e7757e6ce7e6f338
Nicholas Tolppi <ntolppi@gmail.com>
2023-12-14 02:16:21 -0500

    Updated getting list of integers adjacent to each other

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -23,11 +23,12 @@ fun main() {
     val result: MutableList<Int> = mutableListOf()
 
     // Loop through each line of input
-    for (i in inputList.indices) {
+    val numList: MutableList<MutableList<Char>> = mutableListOf()
+    for (y in inputList.indices) {
         // Loop through each Char in input String
-        for (j in inputList[i].indices) {
+        for (x in inputList[y].indices) {
             // Skip periods and numbers
-            if (inputList[i][j] == '.' || inputList[i][j].isDigit()) continue
+            if (inputList[y][x] == '.' || inputList[y][x].isDigit()) continue
 
             /*
             Special character, check for numbers
@@ -35,22 +36,95 @@ fun main() {
             (x - 1, y)     | (x, y)     | (x + 1, y)
             (x - 1, y + 1) | (x, y + 1) | (x + 1, y + 1)
              */
-            val coordList: List<Pair<Int, Int>> = getAdjacentCoords(i, j)
-            coordList.forEach{ coord ->
-                // Check each Pair compared to inputList[i][j]
+            var xHasPrev: Boolean = x - 1 >= 0
+            val yHasPrev: Boolean = y - 1 >= 0
+
+            var xHasNext: Boolean = x + 1 < inputList[y].length
+            val yHasNext: Boolean = y + 1 < inputList.size
+
+            val coordsList: MutableList<Pair<Int, Int>> = mutableListOf()
+
+            var char: Char
+            if (yHasPrev) {
+                char = inputList[y - 1][x]
+                if (char.isDigit())
+                    coordsList.add(Pair(y - 1, x))
             }
+
+            if (yHasNext) {
+                char = inputList[y + 1][x]
+                if (char.isDigit())
+                    coordsList.add(Pair(y + 1, x))
+            }
+
+            if (xHasPrev) {
+                char = inputList[y][x - 1]
+                if (char.isDigit())
+                    coordsList.add(Pair(y, x - 1))
+
+                if (yHasPrev) {
+                    char = inputList[y - 1][x - 1]
+                    if (char.isDigit())
+                        coordsList.add(Pair(y - 1, x - 1))
+                }
+
+                if (yHasNext) {
+                    char = inputList[y + 1][x - 1]
+                    if (char.isDigit())
+                        coordsList.add(Pair(y + 1, x - 1))
+                }
+
+            }
+
+            if (xHasNext) {
+                char = inputList[y][x + 1]
+                if (char.isDigit())
+                    coordsList.add(Pair(y, x + 1))
+
+                if (yHasPrev) {
+                    char = inputList[y - 1][x + 1]
+                    if (char.isDigit())
+                        coordsList.add(Pair(y - 1, x + 1))
+                }
+
+                if (yHasNext) {
+                    char = inputList[y + 1][x + 1]
+                    if (char.isDigit())
+                        coordsList.add(Pair(y + 1, x + 1))
+                }
+            }
+
+            coordsList.forEach { coord ->
+                // TODO: Add list of coords found and skip if found already
+                val numsFound: MutableList<Char> = mutableListOf(inputList[coord.first][coord.second])
+                xHasPrev = coord.second - 1 >= 0
+                xHasNext = coord.second + 1 < inputList[y].length
+
+                var second: Int = coord.second
+                while (xHasPrev && inputList[coord.first][second].isDigit()) {
+                    second -= 1
+                    xHasPrev = second - 1 >= 0
+
+                    if (inputList[coord.first][second].isDigit())
+                        numsFound.addFirst(inputList[coord.first][second])
+                }
+
+                second = coord.second
+                while (xHasNext && inputList[coord.first][second].isDigit()) {
+                    second += 1
+                    if (inputList[coord.first][second].isDigit())
+                        numsFound.addFirst(inputList[coord.first][second])
+                    xHasNext = second + 1 < inputList[y].length
+                }
+
+                numList.add(numsFound)
+                // val numFound: Int = numsFound.joinToString().toInt()
+                println("$coord $numsFound")
+            }
+
         }
     }
 
+    println(numList)
     println(result.sum())
-}
-
-fun getAdjacentCoords(x: Int, y: Int): List<Pair<Int, Int>> {
-    // TODO: Change to each character at coords instead of returning coords
-    // List<Char>
-    return listOf(
-        Pair(x - 1, y - 1), Pair(x, y - 1), Pair(x + 1, y - 1),
-        Pair(x - 1, y),     Pair(x, y),     Pair(x + 1, y),
-        Pair(x - 1, y + 1), Pair(x, y + 1), Pair(x + 1, y + 1),
-    )
 }
