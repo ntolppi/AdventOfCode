da939187c2ae9966137a71fb00b354759a77c89d
Nicholas Tolppi <ntolppi@gmail.com>
2023-12-24 01:56:29 -0500

    Started day 9 part 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,71 +1,39 @@
 package net.nzti
 
-
 fun main() {
-    val inputList: MutableList<String> = readInput("day8_input.txt")
+    val inputList: MutableList<String> = readInput("day9_input.txt")
 
-    val locations: MutableList<String> = mutableListOf()
-    val locationMap: MutableMap<String, Pair<String, String>> = mutableMapOf()
-    inputList.subList(2, inputList.size).forEach { location ->
-        val locationAndDirections: List<String> = location.split(" = ")
-
-        // NQT = (TXC, RVJ)
-        locations.add(locationAndDirections[0])
-        locationMap[locationAndDirections[0]] = locationAndDirections[1].substring(1, locationAndDirections[1].length - 1).split(", ").zipWithNext()[0]
-    }
-
-    val directions: String = inputList[0]
-    var steps: Long = 0
-
-    val startingLocations: MutableList<String> = locations.filter { it[it.length - 1] == 'A' }.toMutableList()
-    val currentLocations: MutableList<String> = locations.filter { it[it.length - 1] == 'A' }.toMutableList()
-
-    val locationsFound: MutableMap<String, Set<Long>> = mutableMapOf()
-    startingLocations.forEach { startingLocation ->
-        locationsFound[startingLocation] = setOf()
-    }
-
-    // while (currentLocations.filter { it[it.length - 1] == 'Z' }.size != currentLocations.size) {
-    while (locationsFound.values.minOf { it.size } < 2) {
-        // Get direction based on current step
-        val currentStep: Int = (steps % directions.length).toInt()
-        steps += 1
-        val dir: Char = directions[currentStep]
-        for (idx in currentLocations.indices) {
-            val location: String = currentLocations[idx]
-            val destination: Pair<String, String> = locationMap.getOrDefault(location, Pair("-1", "-1"))
-            when (dir) {
-                'L' -> {
-                    currentLocations[idx] = destination.first
-                }
-                'R' -> {
-                    currentLocations[idx] = destination.second
-                }
-            }
-
-            if (currentLocations[idx][currentLocations[idx].length - 1] == 'Z') {
-                val startingLocation: String = startingLocations[idx]
-                val currentPeriodList: MutableSet<Long> = locationsFound.getOrDefault(startingLocation, setOf()).toMutableSet()
-                currentPeriodList.add(steps)
-                locationsFound[startingLocation] = currentPeriodList
-                println("${startingLocations[idx]} ${currentLocations[idx]} $currentStep")
-                println("$currentLocations $steps")
-            }
+    val numsList: MutableList<MutableList<Int>> = mutableListOf()
+    inputList.forEach { readings ->
+        val numList: MutableList<Int> = mutableListOf()
+        readings.split(' ').forEach { reading ->
+            numList.add(reading.toInt())
         }
+        numsList.add(numList)
     }
-    println(steps)
-    println(currentLocations)
 
-    val locationsPeriod: MutableList<Long> = mutableListOf()
-    locationsFound.forEach { locationFound ->
-        println(locationFound)
-        val locationSteps: List<Long> = locationFound.value.toList()
-        locationsPeriod.add(locationSteps[1] - locationSteps[0])
+    val nextValList: MutableList<Int> = mutableListOf()
+    numsList.forEach { numList ->
+        nextValList.add(getNextValue(numList))
     }
-    println(locationsPeriod)
+    println(nextValList)
+    println(nextValList.sum())
+}
 
-    // val locationsPeriodMap: MutableMap<Long, MutableList<Long>> = mutableMapOf()
-    // locationsPeriod.forEach { locationPeriod ->
-    //     locationsPeriodMap[locationPeriod] = 0
-    // }
+fun getNextValue(numList: MutableList<Int>): Int {
+    val diffList: MutableList<Int> = mutableListOf()
+
+    for (idx in 0..<numList.size - 1) {
+        diffList.add(numList[idx + 1] - numList[idx])
+    }
+
+    // All zeroes, return 0
+    if (diffList.toSet().size == 1 && diffList.last() == 0) {
+        println(numList + numList.last())
+        return numList.last()
+    }
+
+    val nextVal: Int = numList.last() + net.nzti.solutions.day9.getNextValue(diffList)
+    println(numList + nextVal)
+    return nextVal
 }
