ae7725e22a34ba71dfb850fbfd76ad4a19084422
ntolppi <ntolppi@gmail.com>
2023-12-25 22:20:25 -0500

    WIP Day 11 Part 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,8 @@
 package net.nzti
 
-import io.ktor.http.cio.*
-import kotlin.math.exp
-
 
 fun main() {
-    val inputList: MutableList<String> = readInput("day11_input.txt")
+    val inputList: MutableList<String> = readInput("day11_example_input.txt")
 
     val rowsWithGalaxies: MutableSet<Int> = mutableSetOf()
     val columnsWithGalaxies: MutableSet<Int> = mutableSetOf()
@@ -24,54 +21,55 @@ fun main() {
 
     val expandedGalaxy: MutableList<String> = mutableListOf()
     var prvIdx = 0
-    for (idx in rowsWithNoGalaxies.indices) {
-        expandedGalaxy.addAll(inputList.subList(prvIdx, idx + 1))
-        prvIdx = idx
+    rowsWithNoGalaxies.forEach { row ->
+        expandedGalaxy.addAll(inputList.subList(prvIdx, row + 1))
+        prvIdx = row
     }
     expandedGalaxy.addAll(inputList.subList(prvIdx, inputList.size))
 
-    val columnsWithNoGalaxies: Set<Int> = inputList[0].indices.toSet() subtract columnsWithGalaxies
+    val columnsWithNoGalaxies: List<Int> = (inputList[0].indices.toSet() subtract columnsWithGalaxies).toList()
     expandedGalaxy.forEachIndexed { idx, row ->
         prvIdx = 0
-        var expandedColumn: String = ""
-        for (cidx in columnsWithNoGalaxies.indices) {
-            expandedColumn += row.substring(prvIdx, cidx + 1)
-            prvIdx = cidx
+        var expandedColumn = ""
+        columnsWithNoGalaxies.forEach { column ->
+            expandedColumn += row.substring(prvIdx, column + 1)
+            prvIdx = column
         }
-        expandedColumn += row.substring(prvIdx, inputList[0].length)
+        expandedColumn += row.substring(prvIdx, expandedGalaxy[idx].length)
         expandedGalaxy[idx] = expandedColumn
     }
 
     expandedGalaxy.forEach {
         println(it)
     }
-    println(inputList.size)
-    println(expandedGalaxy.size)
-    println(rowsWithNoGalaxies)
+    println("Number of rows: ${inputList.size}")
+    println("Number of rows expanded: ${expandedGalaxy.size}")
+    println("Rows with no galaxy: $rowsWithNoGalaxies")
 
-    println(inputList[0].length)
-    println(expandedGalaxy[0].length)
-    println(columnsWithNoGalaxies)
+    println("Number of columns: ${inputList[0].length}")
+    println("Number of columns expanded: ${expandedGalaxy[0].length}")
+    println("Columns with no galaxy: $columnsWithNoGalaxies")
 
-    println(numGalaxies)
+    println("Number of galaxies: $numGalaxies")
 
     // Get coords for each galaxy expanded
     val coordsGalaxies: MutableList<Pair<Int, Int>> = mutableListOf()
 
-    expandedGalaxy.forEachIndexed() { ridx, row ->
+    expandedGalaxy.forEachIndexed { ridx, row ->
         row.forEachIndexed { cidx, column ->
             if (column == '#')
                 coordsGalaxies.add(Pair(ridx, cidx))
         }
     }
-    println(coordsGalaxies)
-    println(coordsGalaxies.size)
-    println(((coordsGalaxies.size * coordsGalaxies.size) + coordsGalaxies.size)/2)
+    println("Galaxy coords: $coordsGalaxies")
+    println("Number of galaxies expanded: ${coordsGalaxies.size}")
 
+    val numGalaxyPairs: Int = ((coordsGalaxies.size - 1) * (coordsGalaxies.size)) / 2
+    println("Number of galaxy combinations: $numGalaxyPairs")
 
     coordsGalaxies.forEachIndexed { idx1, coord1 ->
         coordsGalaxies.subList(idx1 + 1, coordsGalaxies.size).forEachIndexed { idx2, coord2 ->
-
+            println("$coord1 $coord2")
         }
     }
 
